664e6b6d3d2560224a8ad48cde4f439392d309e6
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-10 13:06:45 +0530

    Update Day1

---
+++ Day1/Program/Datatypes_input().py
@@ -0,0 +1,26 @@
+# Storing different types of data in variables
+integer_val = 42 #int
+float_val = 3.14 #float
+complex_val = 2 + 3j #complex
+str_val = "Hello, Python!" #str
+bool_val = True #bool
+
+# Example of input and type casting
+user_input = input("Enter an integer: ")  # Accept integer input from user
+user_int = int(user_input)  # Convert input to integer
+
+# Multiple assignments
+x=y=z= 100 #Multiple assignments for sane value
+a, b, c = 10, 20, 30  # Multiple assignments for different values
+
+# Printing stored values along with data types
+print("Stored Integer:", integer_val, "| Type:", type(integer_val))
+print("Stored Float:", float_val, "| Type:", type(float_val))
+print("Stored Complex:", complex_val, "| Type:", type(complex_val))
+print("Stored String:", str_val, "| Type:", type(str_val))
+print("Stored Boolean:", bool_val, "| Type:", type(bool_val))
+
+print("User Input (Integer):", user_int, "| Type:", type(user_int))
+
+print("Multiple Assignment Values - x:", x,", y:",y,", z:",z)
+print("Multiple Assignment Values - a:", a,", b:",b,", c:",c)
