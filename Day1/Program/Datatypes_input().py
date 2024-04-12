93da39a60d5b7d605c8484a1fd0c988975335635
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-12 08:05:42 +0530

    Update Datatypes_input().py

--- Day1/Program/Datatypes_input().py
+++ Day1/Program/Datatypes_input().py
@@ -5,14 +5,6 @@ complex_val = 2 + 3j #complex
 str_val = "Hello, Python!" #str
 bool_val = True #bool
 
-# Example of input and type casting
-user_input = input("Enter an integer: ")  # Accept integer input from user
-user_int = int(user_input)  # Convert input to integer
-
-# Multiple assignments
-x=y=z= 100 #Multiple assignments for sane value
-a, b, c = 10, 20, 30  # Multiple assignments for different values
-
 # Printing stored values along with data types
 print("Stored Integer:", integer_val, "| Type:", type(integer_val))
 print("Stored Float:", float_val, "| Type:", type(float_val))
@@ -20,7 +12,15 @@ print("Stored Complex:", complex_val, "| Type:", type(complex_val))
 print("Stored String:", str_val, "| Type:", type(str_val))
 print("Stored Boolean:", bool_val, "| Type:", type(bool_val))
 
+# Example of input and type casting
+user_input = input("Enter an integer: ")  # Accept integer input from user
+user_int = int(user_input)  # Convert input to integer
+
 print("User Input (Integer):", user_int, "| Type:", type(user_int))
+
+# Multiple assignments
+x=y=z= 100 #Multiple assignments for sane value
+a, b, c = 10, 20, 30  # Multiple assignments for different values
 
 print("Multiple Assignment Values - x:", x,", y:",y,", z:",z)
 print("Multiple Assignment Values - a:", a,", b:",b,", c:",c)
