91758c55c2c168f624f4f72c2255715f6bf65bed
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-18 18:47:53 +0530

    Create string_module.py

---
+++ Day7/Programs/string_module.py
@@ -0,0 +1,7 @@
+import string #importing string module
+
+print("String punctuation : ",string.punctuation) #puctuation : returns punctuation values
+
+print("String ascii letters :",string.ascii_letters) #ascii letters : returns all alphabets including upper & lower case
+
+print("String Digits :",string.digits) #returns numbers from 0 to 9
