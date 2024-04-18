c2b06002de6271869b57eed87747a599227b5b70
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-18 19:27:48 +0530

    Update functions.py

--- Day7/Programs/functions.py
+++ Day7/Programs/functions.py
@@ -1,18 +1,18 @@
-#Functons, return, Normal arguments
+#functions, return, normal arguments, Parameters
 
-def function1(): #with no arguments
+def function1(): #with no parameters
 	print("Function 1 Called!")
 	
-def function2(v):#function with integer argument
-	print(f"Function 2 Called with {v} argument!")
+def function2(v):#function with integer parameter
+	print(f"Function 2 Called with {v} parameter!")
 	
-def function3(name):#functon with string argument&retrun
-	print(f"Function Called with {name} argument return Value :")
+def function3(name):#functon with string parameter &retrun
+	print(f"Function Called with {name} parameter and return Value :")
 	return name.upper() #string return
 
 #Function Calling
 function1()
 
-function2(10)
+function2(10) #integer argument
 
-print(function3("charan"))
+print(function3("charan")) #string argument
