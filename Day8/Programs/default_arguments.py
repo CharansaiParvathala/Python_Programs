048f80264b65106023dc02d55f6dc98623161be9
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-19 16:07:45 +0530

    Delete Day8/Programs/default_arguments.py

--- Day8/Programs/default_arguments.py
+++
@@ -1,9 +0,0 @@
-def function(n_arg, d_arg1 = "Hi", d_arg2 = "how are you ?"):
-	print(d_arg1, n_arg)
-	print(d_arg2)
-
-function("Charan") #call function without passing default arguments
-
-print()
-
-function("Charan","Hello","What are you doing ?") #call function by passing default arguments
