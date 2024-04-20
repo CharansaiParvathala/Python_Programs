86e99b5fe5c5890b6652bcd63ca4a38b5bf8f3df
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-20 11:04:44 +0530

    Create print().py

---
+++ Day9/Programs/print().py
@@ -0,0 +1,23 @@
+
+print("Hi") #passing string parameter
+
+print("Hello") #once we use print it will go to new line at the end
+#\n if we use \n it will go to new line
+
+#its because there is a default argument end = "\n"
+#we can change it if we want
+print("Changing end =! ")
+print("Hi",end="! ")
+print("Hello")
+
+print("my name is","charan")
+
+#similary we have default argument sep = " "
+
+print("Changing sep = : ")
+
+print("my name is","charan",sep=" : ")
+
+#Bonus : we can iterate a string by multiplying it with integer using *
+
+print("\nString"*3)
