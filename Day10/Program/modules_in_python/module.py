209d8a3751dcad7436b1995aaee9a558ea217557
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-21 13:56:30 +0530

    Create module.py

---
+++ Day10/Program/modules_in_python/module.py
@@ -0,0 +1,8 @@
+#python modules are helpful to reuse the code like functions
+#but the difference is module can use another program functions
+
+def function1():
+  print("function one called")
+def function2(a):
+  print("function two called and it return",a,"square")
+  return a**2
