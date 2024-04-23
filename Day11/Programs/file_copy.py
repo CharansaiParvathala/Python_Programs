546d7d8b5b0422640942d6a856229162af387711
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-23 20:03:49 +0530

    Update file_copy.py

--- Day11/Programs/file_copy.py
+++ Day11/Programs/file_copy.py
@@ -1,10 +1,14 @@
 import shutil as s
+import os
 
 filepath = 'file.txt'
 
 copyfilepath = '/storage/emulated/0/Documents/Pydroid3/Python_Programs/copied_with_copyfile.txt'
 
 s.copyfile(filepath,copyfilepath)
+
+if os.path.exists(copyfilepath):
+	print("File copied succesfully.")
 
 '''
 copyfile : only cpoies file content
