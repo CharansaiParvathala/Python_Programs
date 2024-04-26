16c7c9cb478e5e799ace1e58be7cfcfca0207267
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2024-04-26 20:09:10 +0530

    Update Day13

---
+++ Day13/Theory/inheritance&variables.py
@@ -0,0 +1,20 @@
+🔸 Understanding Class Variables and Instance Variables
+
+In 🔸python, variables defined within a class can either be class variables or instance variables, which are fundamental for object-oriented programming.
+
+🔸 Class Variables:
+- Class variables are shared among all instances of a class.
+- They are defined within the class but outside any instance method.
+- They are accessed using the class name.
+
+🔸 Instance Variables:
+- Instance variables are unique to each instance of a class.
+- They are defined inside the class's methods, typically within the `__init__` method.
+- They represent the state of each object and vary from one instance to another.
+
+🔸 Understanding Single Inheritance in 🔸python
+
+Single inheritance in 🔸python refers to the concept of a class inheriting attributes and methods from a single parent class, which is key to building class hierarchies.
+
+🔸 Creating a Subclass:
+- To implement single inheritance, define a new class that inherits from a single parent class.
